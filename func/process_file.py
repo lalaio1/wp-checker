f890d1dc4f6077169660e91933e037b3907a1909
lalaio1 <122741162+lalaio1@users.noreply.github.com>
2024-10-26 20:55:08 -0300

    Update process_file.py

--- func/process_file.py
+++ func/process_file.py
@@ -4,19 +4,33 @@ import re
 
 from func.imports.init import *
 
+# -== Falhas previnidas 
 def process_file(args):
-    total_lines = sum(1 for _ in open(args.file, 'r', encoding='utf-8'))
     results = []
+    
+    try:
+        with open(args.file, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+    except UnicodeDecodeError:
+        with open(args.file, 'r', encoding='ISO-8859-1') as file:
+            lines = file.readlines()
+    except FileNotFoundError:
+        print(f"Erro: O arquivo '{args.file}' não foi encontrado.")
+        return []
+    except Exception as e:
+        print(f"Erro ao abrir o arquivo: {e}")
+        return []
+
+    total_lines = len(lines)
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=args.threads) as executor:
         futures = []
         
-        with open(args.file, 'r') as file:
-            for line in file:
-                match = re.match(r'(https?://[^\s:]+):([^:]+):(.+)$', line.strip())
-                if match:
-                    url, username, password = match.groups()
-                    futures.append(executor.submit(process_url, args, url, username, password))
+        for line in lines:
+            match = re.match(r'(https?://[^\s:]+):([^:]+):(.+)$', line.strip())
+            if match:
+                url, username, password = match.groups()
+                futures.append(executor.submit(process_url, args, url, username, password))
         
         with alive_bar(len(futures), title='Processing') as bar:
             for future in concurrent.futures.as_completed(futures):
